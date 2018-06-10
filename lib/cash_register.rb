b833cc3c3a43ed47d91cb530ce091cdb72eb3169
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-10 18:10:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,12 +14,14 @@ attr_accessor :total, :discount, :title, :price
 
   def apply_discount
     if discount != 0
-      self.total = total * 0.8
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{self.total.to_i}."
     else
       "There is no discount to apply."
     end
   end
+
+  def items
 
 
 end
